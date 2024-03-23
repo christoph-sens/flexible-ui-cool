a23d97461011ae81812d940daa063669c543b7dd
Christoph Sens <christoph.sens@gmail.com>
2024-03-23 11:16:44 -0500

    fixed tests

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app.routes';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
@@ -8,7 +10,8 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuComponent]
+      imports: [MenuComponent],
+      providers:[provideRouter(routes)]
     })
     .compileComponents();
     
