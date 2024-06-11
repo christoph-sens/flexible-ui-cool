96a3117040dd25beb7634e607489c046603e0fef
Christoph Sens <christoph.sens@gmail.com>
2024-06-11 21:24:29 +0200

    updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { importProvidersFrom } from '@angular/core';
-import {} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+	providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi())]
 
 };
